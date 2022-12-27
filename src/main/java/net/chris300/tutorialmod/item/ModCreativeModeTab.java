612c663412fe9a3d64a5449d62f40a92bd0d2aa6
BuildTools <unconfigured@null.spigotmc.org>
2022-12-27 13:20:58 +0100

    Items, Blocks

---
+++ src/main/java/net/chris300/tutorialmod/item/ModCreativeModeTab.java
@@ -0,0 +1,14 @@
+package net.chris300.tutorialmod.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+public class ModCreativeModeTab {
+	public static final CreativeModeTab TUTORIAL_TAB = new CreativeModeTab("tutorialtab") {
+		@Override
+		public @NotNull ItemStack makeIcon() {
+			return new ItemStack(ModItems.RAW_ZIRCON.get());
+		}
+	};
+}
